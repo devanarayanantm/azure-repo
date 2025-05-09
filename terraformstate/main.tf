a921f07edeca49ac4dc2d3e3422ec1f135f14647
devanarayanantm <tmdevanarayanan@gmail.com>
2025-05-09 06:26:28 +0000

    statefile added

--- terraformstate/main.tf
+++ terraformstate/main.tf
@@ -18,10 +18,6 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "state-demo-secure" {
-  name     = "devnstate-demo"
-  location = "centralindia"
-}
 
 resource "random_string" "resource_code" {
   length  = 5
@@ -51,4 +47,78 @@ resource "azurerm_storage_container" "tfstate" {
   name                  = "dev-tfstate-blob"
   storage_account_name  = azurerm_storage_account.tfstate.name
   container_access_type = "private"
+}
+
+
+
+resource "azurerm_resource_group" "example" {
+  name     = "devnstate-demo"
+  location = "centralindia"
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "dev-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+ 
+resource "azurerm_subnet" "example" {
+  name                 = "dev-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+ 
+resource "azurerm_public_ip" "public_ip" {
+  name                = "dev-public-ip"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  allocation_method = "Static"
+}
+ 
+resource "azurerm_network_interface" "example" {
+  name                = "dev-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+ 
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  }
+}
+ 
+ 
+resource "azurerm_linux_virtual_machine" "example" {
+  name                = "dev-machine"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+ 
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+ 
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+ 
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+ 
+  tags = {
+    Name = "dev"
+  }
 }
